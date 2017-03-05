6cb3c61c170f6cb9af4932dae70e2a5cb386d9b3
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-03-06 00:53:38 +0200

    sync module is finished
    added the needed functionality to sync module

--- command.py
+++ command.py
@@ -15,24 +15,6 @@ import unicodedata
 import firebase
 import data
 
-def getCommand(): #called when prog start, connectivity returns, subscribe event
-	rcvData = firebase.get(data.commandURL) #get commands from FDB
-	if rcvData is not None: #if the class is empty the parsed data is None
-		data.waitForCmd = True
-		firebase.put(data.commandURL, {}) #delete commands after parsing
-		io = StringIO
-		commands = json.dumps(jsonData, io) #convert data to string instead of list
-		commands = commands[1:-1] #removes [] from data
-		commands = commands.split(', {') #split string to JSON objects style #JSON.loads accepts only one object at a time
-		commandArray = []
-		for row in commands:
-			temp = json.loads('{' + row) #create JSON object
-			cmd = temp.get('cmd') #get value by key
-			uncoded = unicodedata.normalize('NFKD', cmd).encode('ascii','ignore')
-			commandArray.append(uncoded) #the returned value is unicode 
-		execCommand(commandArray) #execute the commands "sends array of parsed commands"
-	data.waitForCmd = False
-
 def execCommand(rCommand): #each command in the array should be "command,arg1,arg2,..."
 	for row in rCommand:
 		commandList = rCommand.split(",")
@@ -49,6 +31,24 @@ def execCommand(rCommand): #each command in the array should be "command,arg1,ar
 			billsArray = [int(i) for i in r]
 			hardwareDispense(billsArray)
 			openDoor()
+
+def getCommand(): #called when prog start, connectivity returns, subscribe event
+	rcvData = firebase.get(data.commandURL) #get commands from FDB
+	if rcvData is not None: #if the class is empty the parsed data is None
+		data.waitForCmd = True
+		firebase.put(data.commandURL, {}) #delete commands after parsing
+		io = StringIO
+		commands = json.dumps(jsonData, io) #convert data to string instead of list
+		commands = commands[1:-1] #removes [] from data
+		commands = commands.split(', {') #split string to JSON objects style #JSON.loads accepts only one object at a time
+		commandArray = []
+		for row in commands:
+			temp = json.loads('{' + row) #create JSON object
+			cmd = temp.get('cmd') #get value by key
+			uncoded = unicodedata.normalize('NFKD', cmd).encode('ascii','ignore')
+			commandArray.append(uncoded) #the returned value is Unicode 
+		execCommand(commandArray) #execute the commands "sends array of parsed commands"
+	data.waitForCmd = False
 
 def commandStart():
 	S = firebase.subscriber(data.commandURL, getCommand)  #when command class changes in FDB it calls getCommand
--- sync.py
+++ sync.py
@@ -9,10 +9,29 @@
 #to receive changes in timetable and sync FDB with SQLite
 
 import data
+import pythonSQL
 
 #updates SQLite db by parsing timetable from FDB, Delete SQLite old timetable data and adds the new data to it
 def syncdb():
-	return
+	rcvData = firebase.get(data.timetableURL) #get timetable from FDB
+	if rcvData is None: #if the class is empty -> empty timetable
+		dbClear() #empty timetable in SQLite
+	if rcvData is not None: #if the class is empty the parsed data is None
+		io = StringIO
+		timetable = json.dumps(jsonData, io) #convert data to string instead of list
+		timetable = timetable[1:-1] #removes [] from data
+		timetable = timetable.split(', {') #split string to JSON objects style #JSON.loads accepts only one object at a time
+		timeArray = [] #for time values
+		drugArray = [] #for drug Array
+		for row in timetable:
+			temp = json.loads('{' + row) #create JSON object
+			time = temp.get('time') #get value by key
+			uncodedTime = unicodedata.normalize('NFKD', time).encode('ascii','ignore')
+			timeArray.append(uncodedTime) #the returned value is Unicode 
+			drug = temp.get('drug') #get value by key
+			uncodedDrug = unicodedata.normalize('NFKD', drug).encode('ascii','ignore')
+			drugArray.append(uncodedDrug) #the returned value is Unicode 
+		dbRefresh(timeArray, drugArray) #empty timetable in SQLite, and adds the new values
 
 def syncNow():
 	data.waitForSync = True
