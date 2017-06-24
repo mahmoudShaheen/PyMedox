1fc5821d5ead4687bdc4c7e1fbcc4e7d1dbaf9c0
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-06-24 04:12:05 +0200

    initial module for arduino related tasks

---
+++ packages/arduino.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#################################
+#				@author: Mahmoud Shaheen			#
+#				 MedicalBox IOT Project				#
+#				   			Arduino							#
+#################################
+
+#functions excuting tasks related to Arduino
+
