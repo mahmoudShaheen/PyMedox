755e46049133cb40f71705b26da83b00c51e6930
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-06-30 23:28:41 +0200

    bill count status added

--- packages/billCount.py
+++ packages/billCount.py
@@ -10,6 +10,8 @@
 
 from pythonSQL import getTotalDayBills, checkBills
 from notification import notEnoughDayBillsNotification
+import firebase
+import data
 
 #checks if bills in warehouse are enough for one day, also updates bill count in fb db
 #if not sends notification to care giver
@@ -18,8 +20,15 @@ def checkDay():
 	enough = checkBills(totalBills)
 	if (not enough):
 		notEnoughDayBillsNotification()
+		updateStatus("fail")
+	if(enough)
+		updateStatus("ok")
 	updateBillCount() #update bill count in firebase database
 
 #updates bill count in firebase database
 def updateBillCount():
 	execCommand("getBillCount")
+
+def updateStatus(state):
+	stateJson = "{ bills :" + state + "}"
+	firebase.patch(data.stateURL, stateJson)
