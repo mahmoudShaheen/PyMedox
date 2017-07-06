8b0c20d5ff02425f2a1324f356f6a5964b197271
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-07-06 20:29:25 +0200

    sync module detect empty timetable

--- packages/sync.py
+++ packages/sync.py
@@ -28,6 +28,13 @@ def syncdb():
 			print "time: " + str(value['time']) + ", billArray: " + str(value['billArray'])
 		refreshTimetable(timeArray, drugArray) #empty timetable in SQLite, and adds the new values
 		checkDay() #checks if bills in warehouse are enough for one day, also updates bill count in fb db
+	if newTimetable is None: #deleted class
+		timeArray = [] #for time values
+		drugArray = [] #for drug Array
+		print "empty timetable"
+		print newTimetable
+		refreshTimetable(timeArray, drugArray) #empty timetable in SQLite, and adds the new values
+		checkDay() #checks if bills in warehouse are enough for one day, also updates bill count in fb db
 
 def syncNow(args):
 	data.waitForSync = True
